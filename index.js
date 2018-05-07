9d353d0eb156b72f07f40da17f826b30e2e78952
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-07 14:39:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ return "Currently serving " + customer + ".";
 
 
 function currentLine (katzDeliLine) {
-if (katzDeliLine.length===0) {
+if (katzDeliLine.length <= 0) {
   return "The line is currently empty.";
 }
 var customerAndNumber = [];
-for (var i=0; i<katzDeliLine.length; i++) {
-customerAndNumber.push(i + 1 + ". " + katzDeliLine[i])
+for (var i=0; i < katzDeliLine.length; i++) {
+customerAndNumber.push((i + 1) + ". " + katzDeliLine[i])
 }
 return "The line is currently: " + customerAndNumber.join(", "); 
 }
